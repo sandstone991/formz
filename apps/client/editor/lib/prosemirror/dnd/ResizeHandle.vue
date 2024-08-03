b2a18f16face60774631b96052cbc315ea561a04
Ahmed Mahmoud Azzam <ahmed195138@feng.bu.edu.eg>
2024-08-03 20:16:54 +0300

    modifed resizing

--- apps/client/editor/lib/prosemirror/dnd/ResizeHandle.vue
+++ apps/client/editor/lib/prosemirror/dnd/ResizeHandle.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import { nodeViewProps } from '@tiptap/vue-3';
 import { useIndex } from '../composables';
+import { clamp } from 'lodash-es';
 
 const props = defineProps(nodeViewProps)
 const index = useIndex(props)
 const prevIndex = computed(() => index.value - 1)
 const currentPos = computed(() => props.getPos() + 1)
-const nodeToTheLeft = computed(() =>{
+const leftSiblingNodePos = computed(() =>{
   const node = props.editor.$pos(currentPos.value).parent?.children[prevIndex.value]
   return node ? node.pos : -1
 })
@@ -19,20 +20,32 @@ const startResizing = (event: MouseEvent) => {
 const stopResizing = () => {
   isResizing.value = false
 }
+function findColumnBlock(node: HTMLElement) {
+  if(node.hasAttribute('data-column-block')) return node
+  return findColumnBlock(node.parentElement!)
+}
 const resize = (event: MouseEvent) => {
   if (!isResizing.value) return
+  const leftSiblingNode = props.editor.$pos(leftSiblingNodePos.value)
+  const currentNode = props.editor.$pos(currentPos.value)
+
+  const element = currentNode.element;
+  const leftSiblingElement = leftSiblingNode.element;
+  if(!element || !leftSiblingElement) return
+  const parent = findColumnBlock(element)
+  if(!parent) return
+  const width = element.offsetWidth
+  const availableWidth = width + leftSiblingElement.offsetWidth
   const dx = event.clientX - mouseStartingPos.value
-  const prevNode = props.editor.$pos(nodeToTheLeft.value).node
-  const currentNode = props.editor.$pos(currentPos.value).node
-  if (!prevNode || !currentNode) return
-  const prevWidth = prevNode.attrs.widthPercentage as number
-  const currentWidth = currentNode.attrs.widthPercentage as number
-  const newPrevWidth = prevWidth + (dx / props.editor.view.dom.clientWidth) * 100
-  const newCurrentWidth = currentWidth - (dx / props.editor.view.dom.clientWidth) * 100
-  if (newPrevWidth < 10 || newCurrentWidth < 10) return
+  const newWidth  = clamp(width - dx, 30, availableWidth - 30)
+  const newLeftSiblingWidth = availableWidth - newWidth
+  const parentWidth = parent!.offsetWidth
+  const newLeftSiblingNodeWidthPercentage = (newLeftSiblingWidth / parentWidth) * 100
+  const newCurrentWidthPercentage = (newWidth / parentWidth) * 100
+  mouseStartingPos.value = event.clientX
   const tr = props.editor.view.state.tr
-  tr.setNodeMarkup(nodeToTheLeft.value - 1, undefined, { widthPercentage: +newPrevWidth.toFixed(2) })
-  tr.setNodeMarkup(currentPos.value -  1, undefined, {  widthPercentage: +newCurrentWidth.toFixed(2) })
+  tr.setNodeMarkup(leftSiblingNodePos.value - 1, undefined, { widthPercentage: +newLeftSiblingNodeWidthPercentage.toFixed(2) })
+  tr.setNodeMarkup(currentPos.value -  1, undefined, {  widthPercentage: +newCurrentWidthPercentage.toFixed(2) })
   props.editor.view.dispatch(tr)
 }
 useEventListener('mousemove', resize)
